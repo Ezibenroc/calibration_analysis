1e57bb5b9a468497888b9788b5187e4c120baa14
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-02-24 15:24:09 +0100

    Add a script for papermill→nbconvert parallel workflow

---
+++ process_all_g5k.py
@@ -0,0 +1,69 @@
+import time
+import asyncio
+
+
+async def run(cmd):
+    '''
+    From https://docs.python.org/3/library/asyncio-subprocess.html
+    '''
+    print(cmd)
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await proc.communicate()
+
+
+async def run_notebook(cluster, parameter):
+    src_notebook = './G5K_non-regression-test.ipynb'
+    dst_notebook = f'/tmp/{cluster}_{parameter}.ipynb'
+    await run(f'papermill {src_notebook} {dst_notebook} -p cluster {cluster} -p factor {parameter}')
+    return dst_notebook
+
+
+async def convert_notebook(filename):
+    assert filename.endswith('.ipynb')
+    dstname = filename[:-len('.ipynb')] + '.html'
+    await run(f'jupyter nbconvert {filename} --output {dstname}')
+    return dstname
+
+
+async def process_notebook(cluster, parameter):
+    tmp_file = await run_notebook(cluster, parameter)
+    return await convert_notebook(tmp_file)
+
+
+async def process_all(clusters, parameters):
+    await asyncio.gather(*[
+        process_notebook(clust, param)
+        for clust in clusters
+        for param in parameters
+    ])
+
+
+def main():
+    clusters = [
+        'dahu',
+        'yeti',
+        'troll',
+        'paravance',
+        'parasilo',
+        'grisou',
+        'gros',
+        'ecotype',
+        'chiclet',
+        'chetemi',
+    ]
+    parameters = [
+        'avg_gflops',
+        'mean_frequency',
+        'mean_temperature'
+    ]
+    t = time.time()
+    asyncio.run(process_all(clusters, parameters))
+    t = time.time() - t
+    print(f'Processed {len(clusters)} clusters for {len(parameters)} parameters in {t:.2f} seconds')
+
+
+if __name__ == '__main__':
+    main()
